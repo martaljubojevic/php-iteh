e7d0c03e8f53621c6c19b33cbb326670b86d7ce9
martaljubojevic <marta.ljubojevic@gmail.com>
2022-06-25 00:28:28 +0200

    Forma za evidentiranje novog putnika na index.php, style.css za css podešavanja

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+    <h1 id="tah1">Turistička agencija - TravelAround</h1>
+
+    <div>
+
+        <div id="divforma">
+
+            <div class="prviredforma">
+
+                <div class="form">
+                    <label class="form-label">Ime i prezime</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form">
+                    <label class="form-label">Broj telefona</label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form">
+                    <label class="form-label">Broj rezervacije</label>
+                    <input type="text" class="form-control">
+                </div>
+
+            </div>
+
+            <div class="drugiredforma">
+
+                <div class="form">
+                    <label class="form-label">Putovanje</label>
+                    <select type="text" class="form-select">
+                    </select>
+                </div>
+
+                <div class="form">
+                    <label class="form-label">Autobus</label>
+                    <select type="text" class="form-select">
+                    </select>
+                </div>
+
+                <div class="form">
+                    <label class="form-label">Vodič</label>
+                    <select type="text" class="form-select">
+                    </select>
+                </div>
+
+            </div>
+
+        </div>
+
+        <button class="btn btn-secondary" id="dodajbutton">DODAJ PUTNIKA</button>
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
